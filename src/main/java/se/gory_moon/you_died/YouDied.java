f6d74117662a5418e664b5d2a6205f1f4321c77c
GoryMoon <gurreja@gmail.com>
2023-01-31 00:51:32 +0100

    Fixed crash when client dying on a modded server without the mod

--- src/main/java/se/gory_moon/you_died/YouDied.java
+++ src/main/java/se/gory_moon/you_died/YouDied.java
@@ -3,17 +3,13 @@ package se.gory_moon.you_died;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.commons.lang3.tuple.Pair;
-import se.gory_moon.you_died.client.Sounds;
 
 @Mod(YouDied.MOD_ID)
 public class YouDied {
     public static final String MOD_ID = "you_died";
 
     public YouDied() {
-        Sounds.SOUNDS.register(FMLJavaModLoadingContext.get().getModEventBus());
-
         ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> "ANY", (remote, isServer) -> true));
     }
 }
--- src/main/java/se/gory_moon/you_died/client/ClientHandler.java
+++ src/main/java/se/gory_moon/you_died/client/ClientHandler.java
@@ -5,6 +5,8 @@ import net.minecraft.client.audio.SimpleSound;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.client.gui.screen.DeathScreen;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -15,6 +17,8 @@ import se.gory_moon.you_died.YouDied;
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = YouDied.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ClientHandler {
 
+    public static SoundEvent DEATH_SOUND = new SoundEvent(new ResourceLocation(YouDied.MOD_ID, "death"));
+
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void onScreenOpen(GuiOpenEvent event) {
         Screen screen = event.getGui();
@@ -23,7 +27,7 @@ public class ClientHandler {
             ClientPlayerEntity player = Minecraft.getInstance().player;
             if (player != null && !(Minecraft.getInstance().screen instanceof DeathScreen)) {
                 event.setGui(new DeathSplashScreen(new DeathScreenWrapper(deathScreen)));
-                Minecraft.getInstance().getSoundManager().play(SimpleSound.forUI(Sounds.DEATH_SOUND.get(), 1.0F, 1.0F));
+                Minecraft.getInstance().getSoundManager().play(SimpleSound.forUI(DEATH_SOUND, 1.0F, 1.0F));
             }
         }
     }
