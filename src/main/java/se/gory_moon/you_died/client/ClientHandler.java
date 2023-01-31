2ed0537d9ed0a13ef08cd805d7f1dc6dd43237af
Gustaf Järgren <gurreja@gmail.com>
2023-01-31 01:51:19 +0100

    Fixed crash when client dying on a modded server without the mod (#23)

--- src/main/java/se/gory_moon/you_died/client/ClientHandler.java
+++ src/main/java/se/gory_moon/you_died/client/ClientHandler.java
@@ -5,6 +5,8 @@ import net.minecraft.client.gui.screens.DeathScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.resources.sounds.SimpleSoundInstance;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ScreenEvent.Opening;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -15,6 +17,8 @@ import se.gory_moon.you_died.YouDied;
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = YouDied.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ClientHandler {
 
+    public static SoundEvent DEATH_SOUND = new SoundEvent(new ResourceLocation(YouDied.MOD_ID, "death"));
+
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void onScreenOpen(Opening event) {
         Screen screen = event.getScreen();
@@ -22,7 +26,7 @@ public class ClientHandler {
             LocalPlayer player = Minecraft.getInstance().player;
             if (player != null && !(Minecraft.getInstance().screen instanceof DeathScreen)) {
                 event.setNewScreen(new DeathSplashScreen(new DeathScreenWrapper(deathScreen)));
-                Minecraft.getInstance().getSoundManager().play(SimpleSoundInstance.forUI(Sounds.DEATH_SOUND.get(), 1.0F, 1.0F));
+                Minecraft.getInstance().getSoundManager().play(SimpleSoundInstance.forUI(DEATH_SOUND, 1.0F, 1.0F));
             }
         }
     }
